5ef7a98b329b0744c7e80f02387579d339e7a693
Madhav Saraf <madhavsaraf183@gmail.com>
2025-01-14 18:06:44 +0530

    Create a script to convert hex to rgb and vice-versa

---
+++ src/scripts/workplace/apps/color-code-converter.js
@@ -0,0 +1,17 @@
+export default class ColorCodeConverter {
+    static convertRGBToHex(r, g, b) {
+        const toHex = (value) => {
+            const hex = value.toString(16).toUpperCase();
+            return hex.length === 1 ? "0" + hex : hex;
+        }
+        return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    }
+
+    static convertHexToRgb(hex) {
+        hex = hex.replace(/^#/, '');
+        const r = parseInt(hex.substring(0, 2), 16);
+        const g = parseInt(hex.substring(2, 4), 16);
+        const b = parseInt(hex.substring(4, 6), 16);
+        return [r, g, b];
+    }
+}
