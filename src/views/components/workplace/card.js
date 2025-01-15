21aa56397c83920b67d16a1c2c69e9d6dbb54ba3
Madhav Saraf <madhavsaraf183@gmail.com>
2025-01-15 19:10:37 +0530

    Reduce card and image size of apps

--- src/views/components/workplace/card.js
+++ src/views/components/workplace/card.js
@@ -9,10 +9,10 @@ export default function Card({sectionName, workplaceSection}) {
                 workplaceSection.map((section, sectionId) => {
                     return(
                         <Col key={sectionId} lg={2} sm={4} xs={6} className="justify-content-center p-1" role="listitem">
-                            <Container className="p-3 card-elevated d-flex flex-column gap-3" style={{minHeight: '300px'}}>
+                            <Container className="p-3 card-elevated d-flex flex-column gap-3" style={{minHeight: '280px'}}>
                                 <Row Card className="flex-grow-1">
                                     <Col lg={12}>
-                                        <img alt="" style={{height: "150px", width: "100%", objectFit: "cover"}} />
+                                        <img alt="" style={{height: "120px", width: "100%", objectFit: "contain"}} src={section.imageURL}/>
                                     </Col>
                                 </Row>
                                 <Row className="flex-grow-1 d-flex flex-column justify-content-between">
