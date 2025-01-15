75d18772d03b2f0036735a20386327ac4abd4098
Madhav Saraf <madhavsaraf183@gmail.com>
2025-01-15 15:51:43 +0530

    Update the card component for apps and games to make it look proper

--- src/views/components/workplace/card.js
+++ src/views/components/workplace/card.js
@@ -9,19 +9,19 @@ export default function Card({sectionName, workplaceSection}) {
                 workplaceSection.map((section, sectionId) => {
                     return(
                         <Col key={sectionId} lg={2} sm={4} xs={6} className="justify-content-center p-1" role="listitem">
-                            <Container className="p-3 card-elevated">
-                                <Row CardclassName="mb-4">
+                            <Container className="p-3 card-elevated d-flex flex-column gap-3" style={{minHeight: '300px'}}>
+                                <Row Card className="flex-grow-1">
                                     <Col lg={12}>
-                                        <img alt="" style={{"height": "100%", "width": "50%"}}/>
+                                        <img alt="" style={{height: "150px", width: "100%", objectFit: "cover"}} />
                                     </Col>
                                 </Row>
-                                <Row>
+                                <Row className="flex-grow-1 d-flex flex-column justify-content-between">
                                     <Col lg={12}>
-                                        <h3 className="h6">{section.title}</h3>
-                                        <p className="text-body-secondary">{section.description}</p>
+                                        <h3 className="h6 m-0">{section.title}</h3>
+                                        <p className="text-body-secondary m-0">{section.description}</p>
                                     </Col>
                                 </Row>
-                                <Row>
+                                <Row className="d-flex align-items-end">
                                     <Col lg={6} sm={6} xs={6} className="p-0">
                                         <Button as={Link} to={`/workplace/${sectionName}/${section.type ? section.type : section.title.toLowerCase()}`} variant="dark" data-type={section.type}>View</Button>
                                     </Col>
