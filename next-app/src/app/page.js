729cdc5f1e0a92c6190bedb1d5f3cb68495bd64f
Madhav Saraf <madhavsaraf183@gmail.com>
2025-02-04 16:36:43 +0530

    Update the index page design

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -1,9 +1,32 @@
-import App from "@/App";
+'use client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PrimaryNavigation from "@/views/components/primary-nav";
+import PortfolioLayout from "@/views/layouts/portfolio";
+import PortfolioNav from '@/views/components/portfolio_nav';
+import Intro from '@/views/sections/page_sections/intro';
+import AboutMe from '@/views/sections/page_sections/about';
+import Education from '@/views/sections/page_sections/education';
+import Skills from '@/views/sections/page_sections/skill';
+import Experience from '@/views/sections/page_sections/experience';
+import Projects from '@/views/sections/page_sections/projects';
+import Contact from '@/views/sections/page_sections/contact';
 
 export default function Home() {
-  return (
-    <div className="">
-      <App/>
-    </div>
-  );
+	return(
+		<PortfolioLayout 
+			primaryNavigation={<PrimaryNavigation/>}
+			mainContent={
+				<>
+					<PortfolioNav/>
+					<Intro/>
+					<AboutMe/>
+					<Education/>
+					<Skills/>
+					<Experience/>
+					<Projects/>
+					<Contact/>
+				</>
+			}
+		/>
+	);
 }
