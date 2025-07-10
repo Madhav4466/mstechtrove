e9546dfedba69c1f4e500af3984bbd6cec80d139
Madhav Saraf <madhavsaraf183@gmail.com>
2025-07-10 20:00:51 +0530

    Extract page num globally and render the page component accordingly

--- react-app/src/views/components/workplace/apps_container.js
+++ react-app/src/views/components/workplace/apps_container.js
@@ -1,19 +1,17 @@
-import React from "react";
 import { Container } from "react-bootstrap";
 import WorkplaceMenu from "./workplace-menu";
 import AppContent from "./app-content";
 import WorkplaceTitle from "./workplace-title";
+import { useSelector } from "react-redux";
 
 export default function AppsContainer({workplace}) {
-    const[selectedIndex, setSelectedIndex] = React.useState(0);
-
-    const handleMenuSelect = (index) => {setSelectedIndex(index)}
+    const pageNum = useSelector((state) => state.workplacePageNum.value);
 
     return(
        <Container className="pt-5 gap-2 d-flex flex-column">
-            <WorkplaceTitle title={workplace.apps[selectedIndex]?.title || "Apps"}/>
-            <WorkplaceMenu workplace={workplace} onSelect={handleMenuSelect}/>
-            <AppContent workplace={workplace} component={workplace.apps[selectedIndex]?.component}/>
+            <WorkplaceTitle title={workplace.apps[pageNum]?.title || "Apps"}/>
+            <WorkplaceMenu workplace={workplace}/>
+            <AppContent workplace={workplace} component={workplace.apps[pageNum]?.component}/>
         </Container>
     );
 }
--- react-app/src/views/components/workplace/game_container.js
+++ react-app/src/views/components/workplace/game_container.js
@@ -3,17 +3,16 @@ import { Container } from "react-bootstrap";
 import WorkplaceMenu from "./workplace-menu";
 import AppContent from "./app-content";
 import WorkplaceTitle from "./workplace-title";
+import { useSelector } from "react-redux";
 
 export default function GameContainer({workplace}) {
-    const[selectedIndex, setSelectedIndex] = React.useState(0);
-
-    const handleMenuSelect = (index) => setSelectedIndex(index);
+    const pageNum = useSelector((state) => state.workplacePageNum.value);
 
     return(
         <Container className="pt-5 gap-2 d-flex flex-column">
-            <WorkplaceTitle title={workplace.games[selectedIndex]?.title || "Games"}/>
-            <WorkplaceMenu workplace={workplace} onSelect={handleMenuSelect}/>
-            <AppContent component={workplace.games[selectedIndex]?.component}/>
+            <WorkplaceTitle title={workplace.games[pageNum]?.title || "Games"}/>
+            <WorkplaceMenu workplace={workplace}/>
+            <AppContent component={workplace.games[pageNum]?.component}/>
         </Container>
     );
 }
