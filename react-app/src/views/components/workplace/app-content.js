31d213e14b82c66e4296cb997ef8f717948ceae6
Madhav Saraf <madhavsaraf183@gmail.com>
2025-07-15 14:52:12 +0530

    Add a primary background to app and add spacings

--- react-app/src/views/components/workplace/app-content.js
+++ react-app/src/views/components/workplace/app-content.js
@@ -2,8 +2,8 @@ import { Col, Row } from "react-bootstrap";
 
 export default function AppContent ({component}) {
     return(
-        <Row className="p-2">
-            <Col lg={12}>
+        <Row className="p-2 justify-content-center align-items-center">
+            <Col lg={12} className="rounded text-light p-5" style={{background: "black"}}>
                 {component && component}
             </Col>
         </Row>
