6616df412dfaccdf42290a6e09acd17f9e53f869
Madhav Saraf <madhavsaraf183@gmail.com>
2025-07-15 14:08:01 +0530

    Get a refreshed component with default props always

--- react-app/src/views/components/workplace/apps_container.js
+++ react-app/src/views/components/workplace/apps_container.js
@@ -3,6 +3,8 @@ import WorkplaceMenu from "./workplace-menu";
 import AppContent from "./app-content";
 import WorkplaceTitle from "./workplace-title";
 import { useSelector } from "react-redux";
+import { Fragment } from "react/jsx-runtime";
+import WorkInProgress from "../work-in-progress";
 
 export default function AppsContainer({workplace}) {
     const {value: pageNum, category} = useSelector((state) => state.workplacePageNum);
@@ -12,7 +14,12 @@ export default function AppsContainer({workplace}) {
        <Container className="pt-5 gap-2 d-flex flex-column">
             <WorkplaceTitle title={workItem?.title || "Apps"}/>
             <WorkplaceMenu workplace={workplace}/>
-            <AppContent workplace={workplace} component={workItem?.component}/>
+            <AppContent 
+                workplace={workplace} 
+                component={workItem?.getComponent ? 
+                    <Fragment key={workItem?.title}>{ workItem.getComponent() }</Fragment> : 
+                    <WorkInProgress/>}
+            />
         </Container>
     );
 }
