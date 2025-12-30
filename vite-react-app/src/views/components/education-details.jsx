444b6d51d34b470072a545fe2e9846b7b70fce3d
= <=>
2025-12-30 17:42:16 +0530

    Update the education card design and add few icons

--- vite-react-app/src/views/components/education-details.jsx
+++ vite-react-app/src/views/components/education-details.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { FaCalendarAlt, FaGraduationCap, FaStar } from "react-icons/fa";
 
 export default function EducationDetails({education}) {
     return(
@@ -11,16 +12,20 @@ export default function EducationDetails({education}) {
                             <Container className="p-3 card-elevated">
                                 <Row>
                                     <Col lg={4} className="px-3 college-university-image">
-                                        <div>
-                                            <img src={edu.image} alt={edu.college} height={"100%"} width={"100%"} style={{"borderRadius": "10px"}}></img>
-                                        </div>
+                                        <img src={edu.image} alt={edu.college} style={{"borderRadius": "10px"}}></img>
                                     </Col>
-                                    <Col lg={8} className="text-start py-2 px-3 education-details">
-                                        <p className="h4">{edu.title}</p>
-                                        <p className="text-body-secondary h6">{edu.college}</p>
-                                        <p className="h6">{edu.courseDuration}</p>
-                                        <p className="h6">CGPA: {edu.cgpa}</p>
-                                        <p className="h6">Grade: {edu.grade}</p>
+                                    <Col lg={8} className="text-start py-2 px-3 education-details d-flex flex-column justify-content-around">
+                                        <Row>
+                                            <p className="h4" role="heading" aria-level="3">{edu.title}</p>
+                                            <p className="text-body-secondary h6">{edu.college}</p>
+                                        </Row>
+                                        <Row>
+                                            <Col>
+                                                <p className="h6 m-0 p-1"><FaCalendarAlt></FaCalendarAlt> {edu.courseDuration}</p>
+                                                <p className="h6 m-0 p-1"><FaStar style={{color: "#aa6a15"}}/> CGPA: {edu.cgpa}</p>
+                                                <p className="h6 m-0 p-1"><FaGraduationCap/> Grade: {edu.grade}</p>
+                                            </Col>
+                                        </Row>
                                     </Col>
                                 </Row>
                             </Container>
