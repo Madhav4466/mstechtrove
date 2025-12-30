7f2a286737965096989418a47af7f41c09a56076
= <=>
2025-12-30 17:38:30 +0530

    Design new timeline design on page

--- vite-react-app/src/views/components/timeline.jsx
+++ vite-react-app/src/views/components/timeline.jsx
@@ -23,23 +23,77 @@ export default function ExperienceTimeline({timeline}) {
         const months = totalMonths % 12;
         return { years, months };
     }
+
+    const newTimelineStyle = (exp) => {
+        const total = computeCompanyExperience(exp.roleSummary);
+        return (
+            <div className="timeline">
+                <details className="timeline-group" open /*style="--timeline-color:#3b82f6;"*/>
+                <summary>
+                    <div className="company-header">
+                    <div>
+                        <h3>{exp.companyName}</h3>
+                        <p>
+                            Total Experience:{" "} {!total.error ? (`${total.years} Years, ${total.months} Months`) : 
+                            (<span className="text-danger">{total.error}</span>)}
+                        </p>
+                    </div>
+                    <span className="chevron"></span>
+                    </div>
+                </summary>
+                    <div className="timeline-content" role="list">
+                        {exp.roleSummary.map((roleDetails, roleId) => {
+                            return (
+                                <div className="role" role="listitem" key={roleId}>
+                                    <div className="role-date">
+                                        <span className="month">{new Date(roleDetails.startDate).toLocaleString('default', { month: 'short' })}</span>
+                                        <span className="day">{new Date(roleDetails.startDate).getDate()}</span>
+                                    </div>
+                                    <span className="time">{new Date(roleDetails.startDate).getFullYear()}-{isNaN(new Date(roleDetails.endDate).getFullYear()) ? "Present" : new Date(roleDetails.endDate).getFullYear()}</span>
+                                    <h4 className="h5 fw-bold">{roleDetails.role}</h4>
+                                    <p>Responsibilities</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </details>
+            </div>
+        );
+    }
+
+    const oldTimelineStyle = (expId, exp) => {
                 return (
                     <Fragment key={expId}>
                         <Row>
                             <p className="h4 text-body-secondary">{exp.companyName}</p>
-                            <p className="text-body-secondary h6">Total Experience: { !error ? `${years} Years, ${months} Months` : <span className="text-danger">{error}</span>}</p>
+                    {(() => {
+                        const total = computeCompanyExperience(exp.roleSummary);
+                        return (
+                            <p className="text-body-secondary h6">
+                                Total Experience:{" "}{!total.error ? (`${total.years} Years, ${total.months} Months`) : 
+                                (<span className="text-danger">{total.error}</span>)}
+                            </p>
+                        )
+                    })()}
                         </Row>
                         <Row>
                             <ul>
-                                { exp.roleSummary.map((roleDetails, roleId) => {
-                                    return(
-                                        <li key={roleId} style={{"--accent-color":roleDetails.color}}>
-                                            <div className="date">{roleDetails.startDate}</div>
+                        {exp.roleSummary.map((roleDetails, roleId) => {
+                        return (
+                            <li
+                            key={roleId}
+                            style={{ "--accent-color": roleDetails.color }}
+                            >
+                            <div className="date">
+                                {roleDetails.startDate}
+                            </div>
                                             <div className="title">{roleDetails.role}</div>
                                             <div className="descr">
                                                 <p>{roleDetails.employmentType}</p>
                                                 <p>{roleDetails.location}</p>
-                                                <p>{roleDetails.startDate}-{roleDetails.endDate}</p>
+                                <p>
+                                {roleDetails.startDate}-{roleDetails.endDate}
+                                </p>
                                             </div>
                                         </li>
                                     );
@@ -47,7 +101,7 @@ export default function ExperienceTimeline({timeline}) {
                             </ul>
                         </Row>
                     </Fragment>
-                )})
+        )
             }
         </Col>
     );
